d829926e99998cf0c4b4d95ee709fd823ed9fee0
jraleman <jraleman@bendiburg.com>
2018-02-20 19:55:43 -0800

    Organized script.js

--- js/script.js
+++ js/script.js
@@ -1,10 +1,20 @@
-function openNav() {
-  document.getElementById("menuNav").style.width = "250px";
-}
+/*
+** Navbar function(s)
+** .............................................................................
+*/
 
 function closeNav() {
   document.getElementById("menuNav").style.width = "0";
 }
+
+function openNav() {
+  document.getElementById("menuNav").style.width = "250px";
+}
+
+/*
+** Slider function(s)
+** .............................................................................
+*/
 
 $(document).ready(function() {
     $('.pgwSlider').pgwSlider();

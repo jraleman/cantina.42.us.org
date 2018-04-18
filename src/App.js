fa7f5d6117b0de9e3b14047f37f43b73c0952df1
jraleman <jraleman@bendiburg.com>
2018-04-17 22:55:28 -0700

    Add all sections of the landing page to App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,22 @@ import React from 'react';
 
 // Sections
 import CantinaBanner from './components/CantinaBanner';
+import CantinaCarte from './components/CantinaCarte';
+import CantinaSlides from './components/CantinaSlides';
+import CantinaRules from './components/CantinaRules';
+import CantinaFAQ from './components/CantinaFAQ';
+import CantinaFooter from './components/CantinaFooter';
 
 class App extends React.Component {
   render() {
     return (
       <div>
         <CantinaBanner />
+        <CantinaCarte />
+        <CantinaSlides />
+        <CantinaRules />
+        <CantinaFAQ />
+        <CantinaFooter />
       </div>
     );
   }

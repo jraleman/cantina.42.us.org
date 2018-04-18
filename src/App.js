4b4e348c0bbd225ca60e72b72697a40c6ebffec4
jraleman <jraleman@bendiburg.com>
2018-04-17 22:36:10 -0700

    Add banner section to App

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
+class App extends React.Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+
+        {/* Banner section */}
+        <section id="banner">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 text-center wow pulse">
+                <a id="#logo" href="#" onclick="playAudio(this.id);">
+                  <img
+                    className="logo"
+                    src="images/logos/logo-white.svg"
+                    alt="cantina-logo"
+                  />
+                </a>
+                <h1>La Cantina</h1>
+                <br />
+                <a href="dashboard.html" className="btn" target="_self">
+                  <i className="fas fa-sign-in-alt" /> Log in
+                </a>
+              </div>
+            </div>
+          </div>
+        </section>
+
       </div>
     );
   }

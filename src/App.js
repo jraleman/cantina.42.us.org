cd63f118047a0b242dd5762d63eacf2d8b32e70d
jraleman <jraleman@bendiburg.com>
2018-04-17 22:54:19 -0700

    Replace banner section code with component tag reference

--- src/App.js
+++ src/App.js
@@ -1,32 +1,13 @@
 import React from 'react';
 
+// Sections
+import CantinaBanner from './components/CantinaBanner';
+
 class App extends React.Component {
   render() {
     return (
       <div>
-
-        {/* Banner section */}
-        <section id="banner">
-          <div className="container">
-            <div className="row">
-              <div className="col-md-12 text-center wow pulse">
-                <a id="#logo" href="#" onclick="playAudio(this.id);">
-                  <img
-                    className="logo"
-                    src="images/logos/logo-white.svg"
-                    alt="cantina-logo"
-                  />
-                </a>
-                <h1>La Cantina</h1>
-                <br />
-                <a href="dashboard.html" className="btn" target="_self">
-                  <i className="fas fa-sign-in-alt" /> Log in
-                </a>
-              </div>
-            </div>
-          </div>
-        </section>
-
+        <CantinaBanner />
       </div>
     );
   }
